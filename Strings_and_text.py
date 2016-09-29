2e271bb7a41e9f4b3bf2904030beb368d1c308b6
Deitsuen <fireblack@orange.fr>
2016-09-29 19:52:04 +0200

    Add files via upload

---
+++ Strings_and_text.py
@@ -0,0 +1,22 @@
+# -*-coding: utf-8 -*-
+x = "J'ai mange %d  poulet aujourd'hui" % 10
+poulet = "ceux qui respectent le poulet"
+autres = "et les autres.."
+y = "Il y a %s,%s" % (poulet, autres)
+
+print x
+print y
+
+print "je dis juste %s." % y
+print "Sinon  %r" % x
+
+vrai = True
+Howmetyoumother = "ahahaha %r story bro true ..."
+
+print Howmetyoumother % vrai
+
+w = "Nan je vais faire "
+
+e = "CACA CHEZ PAUL"
+
+print w + e
